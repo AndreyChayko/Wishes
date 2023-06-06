876988c09f05b050d566546e8f156fdc54917da8
Andrey Chayko <chaiko.work@gmail.com>
2023-06-06 23:19:21 +0200

    added hearts by touch event

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-document.addEventListener('mousemove', (event) => {
-    console.log('Test1111',event);
+document.addEventListener('touchmove', (event) => {
+    const body = document.querySelector('body');
+    const heart = document.createElement('span');
+    const rect = event.target.getBoundingClientRect();
+    const x = event.targetTouches[0].clientX - rect.x;
+    const y = event.targetTouches[0].clientY - rect.y;
+
+    heart.style.left = x + 'px';
+    heart.style.top = y + 'px';
+
+    const size = Math.random() * 80;
+    heart.style.width = 20 + size + 'px';
+    heart.style.height = 20 + size + 'px';
+
+    const transformValue = Math.random() * 360;
+    heart.style.transform = `rotate(${transformValue}deg )`;
+
+    body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 1000)
 })
